50a6cfc7e84f706a95252a45611a10df57a692b9
juanma1023 <juanmanuelxd3577@gmail.com>
2025-02-18 23:50:16 -0500

    Modificacion del modelo de predicción, cambio en las variables no tenidas en cuenta y reajuste de parametros

--- Model/model.py
+++ Model/model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import joblib
 
@@ -10,17 +10,17 @@ class Pollution:
         self.model = None
         self.X_train, self.X_test, self.y_train, self.y_test = None, None, None, None
 
-    def data_split(self):
-        x = self.data.drop(columns = ["pm2.5", "date"])
+    def split_data(self):
+        x = self.data.drop(columns = ["pm2.5", "date","Is" , "Ir","cbwd_NW","cbwd_SE","cbwd_cv"])
         y = self.data["pm2.5"]
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(x, y, test_size = 0.2, random_state= 42)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(x, y, test_size = 0.15, random_state= 42)
         print('Data split for training')
 
-    def model_train(self):
+    def train_model(self):
         if self.X_train is None or self.y_test is None:
             raise ValueError('data not prepared.')
-        self.model = RandomForestRegressor(n_estimators=200, max_depth= 20,
+        self.model = GradientBoostingRegressor(n_estimators=500, max_depth= 10,
                                         min_samples_split= 5, min_samples_leaf= 2, random_state= 42)
         self.model.fit(self.X_train, self.y_train)
         print('model trained')
@@ -28,7 +28,7 @@ class Pollution:
         joblib.dump(self.model, filename ='DataAnalysisQuipux\Model\model_rfr.pkl')
 
     
-    def model_evaluate(self):
+    def evaluate_model(self):
         if self.model is None: 
             raise ValueError('model does not train')
         
@@ -36,8 +36,7 @@ class Pollution:
         mean_abs_error = mean_absolute_error(self.y_test, y_pred)
         mean_sq_error = mean_squared_error(self.y_test, y_pred)
 
-        print(f"evaluation: ")
-        print(f"mean absolute error: {mean_abs_error}")
-        print(f"mean squared error: {mean_sq_error}")
+        print(f"Evaluation: \nMean Absolute Error: {mean_abs_error} \nMean Squared Error: {mean_sq_error}")
+
 
                             
