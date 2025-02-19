d5b421bb4fea5e09df853bb32414ed1de445153b
juanma1023 <juanmanuelxd3577@gmail.com>
2025-02-18 23:51:51 -0500

    Correccion en los nombres de las funciones

--- DataClean/dataClean.py
+++ DataClean/dataClean.py
@@ -7,24 +7,25 @@ class Data:
         self.df = None
         self.null_values = None
     
-    def data_load(self):
+    def load_data(self):
         if not os.path.exists(self.file_path):
             raise FileNotFoundError("File %s not found" % self.file_path)
         self.df = pd.read_csv(self.file_path)
         print("Data loaded")
 
-    def data_clean(self):
+    def clean_data(self):
         self.df['date'] = pd.to_datetime(self.df[["year","month","day","hour"]])
         self.null_values = self.df[self.df["pm2.5"].isnull()].copy()
         self.df.drop(columns=["year","month","day","hour"], inplace= True)
-        self.df['pm2.5'] = self.df["pm2.5"].interpolate(method='linear')
+        self.df["pm2.5"] = self.df["pm2.5"].interpolate(method='linear',limit_direction='both')
         self.df = pd.get_dummies(self.df, columns=['cbwd'], drop_first= True)
         print("Data cleaned")
+        print(self.df)
 
-    def data_get(self):
+    def get_data(self):
         if self.df is None:
             raise ValueError("Data not loaded")
         return self.df
     
-    def data_null_values(self):
+    def null_values_data(self):
         return self.null_values
